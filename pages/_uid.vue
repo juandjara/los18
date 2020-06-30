d0c1e35c5e52c32b3d796753866f6360c820bf3c
Juan D. Jara <juanorigami@gmail.com>
2020-06-30 17:22:21 +0200

    initial commit

---
+++ pages/_uid.vue
@@ -0,0 +1,11 @@
+<template>
+  <slice-zone type="page" :uid="$route.params.uid" />
+</template>
+
+<script>
+import SliceZone from 'vue-slicezone'
+
+export default {
+  components: { SliceZone },
+}
+</script>
